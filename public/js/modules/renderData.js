c720a4687b36cb10d5ff79e584f3d121e013d099
Bas Pieren <bas.pieren@quicknet.nl>
2019-07-17 14:15:55 +0200

    ✨ expanding childs of articles

--- public/js/modules/renderData.js
+++ public/js/modules/renderData.js
@@ -18,20 +18,30 @@ function articles() {
 	dataParse.articles.slice(1).forEach(a => {
 		const articleContainer = document.getElementsByClassName("articles"),
 			article = document.createElement("article"),
+			div = document.createElement("div"),
+			img = document.createElement("div"),
 			h3 = document.createElement("h3"),
+			h4 = document.createElement("h4"),
 			p = document.createElement("p")
 
 		const cleanContent = /(<)([^abc][\w]*)(>)/g
 
 		article.className = "article"
+		img.className = "article__img"
+		div.className = "article__copy-container"
 		h3.textContent = a.title
 		h3.className = "article__title"
+		h4.textContent = a.title
+		h4.className = "article__sub-title"
 		p.textContent = a.content.replace(cleanContent, "")
 		p.className = "article__copy"
 
 		articleContainer[0].appendChild(article)
-		article.appendChild(h3)
-		article.appendChild(p)
+		article.appendChild(img)
+		article.appendChild(div)
+		div.appendChild(h3)
+		div.appendChild(h4)
+		div.appendChild(p)
 	})
 }
 
